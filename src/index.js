477c8e8ff5900066b57eac6d3e6f5859d6415290
Batyr <gilfanov228@gmail.com>
2023-08-17 21:57:39 -0700

    Disabled ESLint during build

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line no-unused-expressions
+process.env.NODE_ENV !== 'production' && require('react-scripts/scripts/start');
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
