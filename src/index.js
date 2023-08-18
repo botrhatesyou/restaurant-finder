72f2361eccc3479b43ac16763c2469f09dea5e41
Batyr <gilfanov228@gmail.com>
2023-08-17 22:05:05 -0700

    Fixed the order of statements

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-// eslint-disable-next-line no-unused-expressions
-process.env.NODE_ENV !== 'production' && require('react-scripts/scripts/start');
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// eslint-disable-next-line no-unused-expressions
+process.env.NODE_ENV !== 'production' && require('react-scripts/scripts/start');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
